357828639a5080b0a68349c008e8c7faa967c463
ray <ormillada.jr@gmail.com>
2024-12-09 17:06:21 +0800

    asd

--- src/components/ReadQR.jsx
+++ src/components/ReadQR.jsx
@@ -5,6 +5,9 @@ import axios from 'axios';
 
 
 const ReadQR = () => {
+
+
+
 	const date = moment();
 	const formattedDate = date.format('dddd, MMM D YYYY, h:mm A');
 	// const [data, setData] = useState('');
@@ -21,31 +24,32 @@ const ReadQR = () => {
 				`http://localhost:8000/api/users/${result[0].rawValue}`
 			);
 			const studentData = await response.json();
-			const student = studentData[0];
+			const student = await studentData[0];
+			// console.log(student)
 
 			// Update display with student data
-			setDisplay(student);
+			setDisplay(studentData);	
 
 			// Prepare student info for posting
-			const studentInfo = {
-				No: student.studentNo,
-				Name: student.studentName,
-				Year: student.studentYear,
-				Course: student.studentCourse,
-			};
+			// const studentInfo = {
+			// 	No: student.studentNo,
+			// 	Name: student.studentName,
+			// 	Year: student.studentYear,
+			// 	Course: student.studentCourse,
+			// };
 
 			setCondition((prevCondition) => !prevCondition);
 
-			// Get current date and time using Moment.js
-			const logAt = formattedDate; // Example format: "Monday, 08-20-2024 11:30"
-			console.log(logAt);
+			// // Get current date and time using Moment.js
+			// const logAt = formattedDate; // Example format: "Monday, 08-20-2024 11:30"
+			// console.log(logAt);
 
-			// Post student info and logAt
-			const postResponse = await axios.post('http://localhost:8000/api/scan/', {
-				studentInfo,
-				logAt,
-			}); // Adjust port if necessary
-			console.log('User created:', postResponse.data);
+			// // Post student info and logAt
+			// const postResponse = await axios.post('http://localhost:8000/api/scan/', {
+			// 	studentInfo,
+			// 	logAt,
+			// }); // Adjust port if necessary
+			// console.log('User created:', postResponse.data);
 		} catch (error) {
 			console.error('Error:', error);
 			// Optionally, show an error message to the user
@@ -54,13 +58,37 @@ const ReadQR = () => {
 	};
 	console.log(Date().toLocaleString())
 
+	const styles = {
+		container: {
+			width: 500,
+			margin: 'auto',
+			positon: 'relative'
+			
+		},
+			controls:{
+				marginBottom: 0
+			}
+	}
 	return (
-			<div className='w-5/6 mx-auto h-full grid place-items-center'>
-		<div className="grid grid-cols-2 w-full p-8">
-			<div className="w-96">
-				<Scanner onScan={handleScan} />
+			<div className=''>
+		<div className="">
+			<div className="" style={styles.container}>
+				<Scanner className="" onScan={handleScan} scanDelay={2000} allowMultiple={true}
+				components={{
+					zoom: true,
+					finder: true,
+					torch: true,
+					audio: true,
+					onOff: true,
+				}} 
+				videoStyle={{
+					width: "100%",
+					height: "auto",
+					objectFit:"cover"
+				}}
+				/>
 			</div>
-			<div className="p-4 border border-zinc-200 shadow rounded-xl flex flex-col gap-3">
+			{/* <div className="p-4 border border-zinc-200 shadow rounded-xl flex flex-col gap-3">
 				<h1>Student Information</h1>
 				<div>
 					<label className="block mb-1.5 text-sm font-medium">ID Number</label>
@@ -108,7 +136,7 @@ const ReadQR = () => {
 					/>
 				</div>
 
-			</div>
+			</div> */}
 
 			{/* <ul>
         {displayId.map(id => (
